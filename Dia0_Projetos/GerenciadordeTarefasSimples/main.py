d92a04c8cf4a6c51e1ee0c3a9398297709b7ea5a
leirisson <leirissonsouza99@gmail.com>
2024-11-27 15:06:28 -0400

    cadastro de tarefas

---
+++ Dia0_Projetos/GerenciadordeTarefasSimples/main.py
@@ -0,0 +1,7 @@
+from modulos import moduloDeTarefas
+
+
+titulo = input("qual a tarefa: ")
+
+
+moduloDeTarefas.gerenciadorDeTarefas(titulo)
---
+++ Dia0_Projetos/GerenciadordeTarefasSimples/modulos/moduloDeTarefas.py
@@ -0,0 +1,7 @@
+
+def gerenciadorDeTarefas(tarefas):
+    tarefas = []
+    tarefa = {}
+    tarefa['titulo'] = tarefa
+    tarefas.append(tarefa)
+    print(tarefas)
